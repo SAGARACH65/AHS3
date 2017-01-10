ae879eed1f904e8c35f7d8ac01162e9397d98b2e
SAGARACH65 <sagarach65@gmail.com>
2017-01-10 11:51:55 +0545

    before changin main activity to single start page from database

---
+++ app/src/main/java/com/example/sagar/ahs/AddNewField.java
@@ -0,0 +1,16 @@
+package com.example.sagar.ahs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddNewField extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_field);
+    }
+}
--- app/src/main/java/com/example/sagar/ahs/MainActivity.java
+++ app/src/main/java/com/example/sagar/ahs/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.sagar.ahs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity{
 
+    public void addNewFields(View view) {
+
+        Intent callnew =new Intent(this,AddNewField.class);
+        startActivity(callnew);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
     }
-    public static void addNewUser()
-    {
 
-    }
     public static void showUserInfo()
     {
 
