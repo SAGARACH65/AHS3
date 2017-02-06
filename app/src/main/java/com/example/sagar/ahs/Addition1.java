e500270be5f6703e51d676a4eadeed33d61dfd34
SAGARACH65 <sagarach65@gmail.com>
2017-02-06 20:15:02 +0545

    some piece of bluetooth connect working

--- app/src/main/java/com/example/sagar/ahs/Addition1.java
+++ app/src/main/java/com/example/sagar/ahs/Addition1.java
@@ -1,6 +1,12 @@
 package com.example.sagar.ahs;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,60 +17,307 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 import static android.content.ContentValues.TAG;
 import static java.lang.Integer.parseInt;
 
 public class Addition1 extends AppCompatActivity {
+    private final static int REQUEST_ENABLE_BT = 1;
 
-    private boolean check=true;
-     private String name;
-     private String Area;
-    private  String M_unit;
+    private boolean check = true;
+    private String name;
+    private String Area;
+    private String M_unit;
     private String Crop_grown;
     private Bundle extras;
     private String harvest_day;
     private String nitromessage;
-    Algorithm al1= new Algorithm(Addition1.this);
-    public void moveToSecond(View view)
-    {
+    Algorithm al1 = new Algorithm(Addition1.this);
+    public BluetoothSocket scSocket;
+
+    private static UUID MY_UUID = UUID.fromString("446118f0-8b1e-11e2-9e96-0800200c9a66");
+//or DEADBEEF-0000-0000-0000-000000000000
+    // The local server socket
+    private BluetoothServerSocket mmServerSocket;
+
+    // based on android.bluetooth.BluetoothAdapter
+    private BluetoothAdapter mAdapter;
+    private BluetoothDevice remoteDevice;
+
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+    //private static final UUID MY_UUID =
+       //     UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // Server's MAC address
+    private static String address = "20:16:03:21:70:44";//20:16:03:21:70:44
+    BluetoothAdapter mBluetoothAdapter;
+    int readBufferPosition;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    public void moveToSecond(View view) {
 
         Intent intent = new Intent(this, Addition2.class);
         extras = new Bundle();
 
-        extras.putString("name",name);
-        extras.putString("Area",Area);
-        extras.putString("Measurement Unit",M_unit);
-        extras.putString("Crop planted",Crop_grown);
-        extras.putString("Growth_End_Date",harvest_day);
-        extras.putString("nitromessage",nitromessage);
+        extras.putString("name", name);
+        extras.putString("Area", Area);
+        extras.putString("Measurement Unit", M_unit);
+        extras.putString("Crop planted", Crop_grown);
+        extras.putString("Growth_End_Date", harvest_day);
+        extras.putString("nitromessage", nitromessage);
         intent.putExtras(extras);
         startActivity(intent);
         finish();
     }
-    public void moveToThird(View view)
-    {
+
+    public void moveToThird(View view) {
 
         Intent intent = new Intent(this, Addition3.class);
         extras = new Bundle();
 
-        extras.putString("name",name);
-        extras.putString("Area",Area);
-        extras.putString("Measurement Unit",M_unit);
-        extras.putString("Crop planted",Crop_grown);
-        extras.putString("Growth_End_Date",harvest_day);
-        extras.putString("nitromessage",nitromessage);
+        extras.putString("name", name);
+        extras.putString("Area", Area);
+        extras.putString("Measurement Unit", M_unit);
+        extras.putString("Crop planted", Crop_grown);
+        extras.putString("Growth_End_Date", harvest_day);
+        extras.putString("nitromessage", nitromessage);
         intent.putExtras(extras);
         startActivity(intent);
         finish();
 
     }
 
-    public void syncDevice(View view)
-    {
-        BlueToothController bc1=new BlueToothController();
+    private interface MessageConstants {
+        public static final int MESSAGE_READ = 0;
+        public static final int MESSAGE_WRITE = 1;
+        public static final int MESSAGE_TOAST = 2;
 
-
+        // ... (Add other message types here as needed.)
     }
+   // private Handler mHandler; // handler that gets info from Bluetooth service
+
+
+
+    public void syncDevice(View view) throws  IOException {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+       // btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+       // btSocket.connect();
+        try {
+            btSocket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
+            btSocket.connect();
+        }catch (Exception e2) {
+                Log.e("", "Couldn't establish Bluetooth connection!");
+            }
+            ConnectedThread b1 = new ConnectedThread(btSocket);
+    }
+    public class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                ///Log.e(TAG, "Error occurred when creating input stream", e);
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                // Log.e(TAG, "Error occurred when creating output stream", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+                    // Send the obtained bytes to the UI activity.
+                    Message readMsg = mHandler.obtainMessage(
+                       MessageConstants.MESSAGE_READ, numBytes, -1,
+                            mmBuffer);
+                    readMsg.sendToTarget();
+
+                } catch (IOException e) {
+
+                    break;
+                }
+            }
+        }
+    }
+       /* UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        //beginListenForData();
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+       // stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() )
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                          //                  myLabel.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        //stopWorker = true;
+                    }
+                }
+            }
+        });
+*/
+
+
+    /*class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating input stream", e);
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "Error occurred when creating output stream", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer);
+                    // Send the obtained bytes to the UI activity.
+                    Message readMsg = mHandler.obtainMessage(
+                            Addition1.MessageConstants.MESSAGE_READ, numBytes, -1,
+                            mmBuffer);
+                    readMsg.sendToTarget();
+
+                } catch (IOException e) {
+                    Log.d(TAG, "Input stream was disconnected", e);
+                    break;
+                }
+            }
+        }
+    }*/
+
+
+
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            byte[] writeBuf = (byte[]) msg.obj;
+            // construct a string from the valid bytes in the buffer
+            String writeMessage = new String(writeBuf, 0, msg.arg1);
+            String seperateValues[] = writeMessage.split(",");
+            String pressure_1 = seperateValues[0];
+            String pressure_2 = seperateValues[1];
+            String pressure_3 = seperateValues[2];
+            String pressure_4 = seperateValues[3];
+            String pressure_5 = seperateValues[4];
+
+            int pressure_1_int = Integer.parseInt(pressure_1);
+            int pressure_2_int = Integer.parseInt(pressure_2);
+            int pressure_3_int = Integer.parseInt(pressure_3);
+            int pressure_4_int = Integer.parseInt(pressure_4);
+            int pressure_5_int = Integer.parseInt(pressure_5);
+
+
+        }
+    };
+
+
+
 
     public void findCrop(View view) {
         //getting the environmental variable values
@@ -243,14 +496,14 @@ public class Addition1 extends AppCompatActivity {
 
         Button button =(Button)findViewById(R.id.button6);
 
-        button.setOnClickListener(new View.OnClickListener() {
+       /* button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent dbmanager = new Intent(getApplicationContext(),AndroidDatabaseManager.class);
                 startActivity(dbmanager);
             }
         });
-        cb1.setOnClickListener(new View.OnClickListener() {
+*/        cb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(check) {
@@ -262,7 +515,7 @@ public class Addition1 extends AppCompatActivity {
                 {
                     tv6.setVisibility(View.INVISIBLE);
                     et1.setVisibility(View.INVISIBLE);
-                        check=true;
+                    check=true;
                 }
                 }
         });
@@ -277,3 +530,4 @@ public class Addition1 extends AppCompatActivity {
 
 
 }
+
--- app/src/main/java/com/example/sagar/ahs/BlueToothController.java
+++ app/src/main/java/com/example/sagar/ahs/BlueToothController.java
@@ -1,26 +1,141 @@
+
 package com.example.sagar.ahs;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.os.ParcelUuid;
+import android.os.Message;
 
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Set;
+import android.os.Handler;
 
-/**
- * Created by sagar on 1/28/2017.
- */
+
 
 public class BlueToothController {
 
-    private OutputStream outputStream;
-    private InputStream inStream;
+   // private static final String TAG = "MY_APP_DEBUG_TAG";
+    private Handler mHandler; // handler that gets info from Bluetooth service
 
-    public void pair() {
-        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+    private interface MessageConstants {
+        public static final int MESSAGE_READ = 0;
 
 
+        // ... (Add other message types here as needed.)
     }
+
+
+    //Handler mHandler = new Handler() ;
 }
+//        @Override
+      /*  public void handleMessage(Message msg) {
+            byte[] writeBuf = (byte[]) msg.obj;
+            // construct a string from the valid bytes in the buffer
+            String writeMessage = new String(writeBuf,0,msg.arg1);
+            String seperateValues[] =  writeMessage.split(",");
+            String pressure_1 = seperateValues[0];
+            String pressure_2 = seperateValues[1];
+            String pressure_3 = seperateValues[2];
+            String pressure_4 = seperateValues[3];
+            String pressure_5 = seperateValues[4];
+
+            int pressure_1_int = Integer.parseInt(pressure_1);
+            int pressure_2_int = Integer.parseInt(pressure_2);
+            int pressure_3_int = Integer.parseInt(pressure_3);
+            int pressure_4_int = Integer.parseInt(pressure_4);
+            int pressure_5_int = Integer.parseInt(pressure_5);
+
+
+        }
+    };*/
+
+/*
+import android.bluetooth.BluetoothSocket;
+import android.os.Message;
+import android.util.Log;
+
+import com.example.sagar.ahs.Addition1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.content.ContentValues.TAG;
+
+
+
+
+
+
+
+
+
+
+
+// private static final String TAG = "MY_APP_DEBUG_TAG";
+//private Handler mHandler; // handler that gets info from Bluetooth service
+        BluetoothSocket socket = null;
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Listen to the server socket if we're not connected
+        // while (true) {
+
+        try {
+            // Create a new listening server socket
+            Log.d((String) this.getTitle(), ".....Initializing RFCOMM SERVER....");
+
+            // MY_UUID is the UUID you want to use for communication
+            mmServerSocket = mAdapter.listenUsingRfcommWithServiceRecord("MyService",    MY_UUID);
+            //mmServerSocket =  mAdapter.listenUsingInsecureRfcommWithServiceRecord(NAME, MY_UUID); // you can also try  using In Secure connection...
+
+            // This is a blocking call and will only return on a
+            // successful connection or an exception
+            socket = mmServerSocket.accept();
+
+        } catch (Exception e) {
+
+        }
+
+        byte[] buffer = new byte[256];  // buffer store for the stream
+        int bytes; // bytes returned from read()
+        try {
+            Log.d((String) this.getTitle(), "Closing Server Socket.....");
+            mmServerSocket.close();
+
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the BluetoothSocket input and output streams
+
+            //tmpIn = socket.getInputStream();
+            //tmpOut = socket.getOutputStream();
+
+            DataInputStream mmInStream = new DataInputStream(tmpIn);
+            DataOutputStream mmOutStream = new DataOutputStream(tmpOut);
+            // here you can use the Input Stream to take the string from the client  whoever is connecting
+            //similarly use the output stream to send the data to the client
+
+            // Read from the InputStream
+            bytes = mmInStream.read(buffer);
+            String readMessage = new String(buffer, 0, bytes);
+            // Send the obtained bytes to the UI Activity
+
+           // String seperateValues[] = readMessage.split(",");
+            String pressure_1 = seperateValues[0];
+            String pressure_2 = seperateValues[1];
+            String pressure_3 = seperateValues[2];
+            String pressure_4 = seperateValues[3];
+            String pressure_5 = seperateValues[4];
+
+            int pressure_1_int = Integer.parseInt(readMessage);
+            //int pressure_2_int = Integer.parseInt(pressure_2);
+            //int pressure_3_int = Integer.parseInt(pressure_3);
+            //int pressure_4_int = Integer.parseInt(pressure_4);
+            //int pressure_5_int = Integer.parseInt(pressure_5);
+        TextView et1= (TextView) findViewById(R.id.textView31);
+            et1.setText(pressure_1_int);
+
+        } catch (Exception e) {
+            //catch your exception here
+        }
+*/        // }
